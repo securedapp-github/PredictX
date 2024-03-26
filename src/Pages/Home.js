fd3c0a97345f15176d060fc2336fcfee172bfa52
Naveen-gn <naveen2003gn@gmail.com>
2024-03-26 21:57:22 +0530

    Predictive Crime Analysis section fully completed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -13,9 +13,9 @@ export default function Home() {
     <div>
       <div className='flex '>
 
-      <div className='h-screen bg-black'><img src={Homeicon} alt="Homeicon" className='absolute hidden sm:block top-0 right-0 h-2/4 w-48 sm:h-4/5  sm:min-w-80 md:h-full md:w-2/4 lg:w-2/5 xl:2/5 2xl:w-1/3'  /></div>
+      <div className='sm:h-screen h-10/12 '><img src={Homeicon} alt="Homeicon" className='absolute hidden sm:block top-0 right-0 h-2/4 w-48 sm:h-4/5  sm:min-w-80 md:h-full md:w-2/4 lg:w-2/5 xl:2/5 2xl:w-1/3'  /></div>
 
-      <div  className='  xl:w-3/4 sm:w-3/5 lg:w-3/5 z-50 sm:ml-3 md:ml-2'>
+      <div  className='  xl:w-3/4 sm:w-3/5 lg:w-3/5 z-50 sm:ml-3 md:ml-2  '>
       <h1 className=' font-bold text-4xl sm:text-2xl sm:ml-4 lg:text-3xl lg:ml-36 xl:text-4xl xl:ml-36 2xl:text-4xl flex flex-col justify-center text-center mt-20 ' style={{color:'rgb(13, 0, 107)'}}> <span>Welcome to the world of <span className='text-red-700'>Predictive</span></span> <span className='text-red-700'>crime analysis</span></h1>
 
 <div className='w-full flex flex-col mt-12 gap-12'>
@@ -61,7 +61,7 @@ export default function Home() {
       </div>
       </div>
 
-      <div className='w-full '>
+      <div className='w-full mt-10 sm:mt-0'>
     <h1 className='font-semibold text-3xl text-center'>Predictive Crime Analysis</h1>
 
     <div className='w-full flex justify-center items-center flex-wrap mt-10 gap-5'>
@@ -69,12 +69,12 @@ export default function Home() {
 
         <div className='flex flex-col gap-5 w-4/5 lg:w-1/2  ' >
           <p className='text-black font-medium text-justify'>
-          PredctX is a revolutionary police performance management and predictive crime analysis tool. We leverage the power of data science and artificial intelligence to equip law enforcement agencies with the insights they need to proactively prevent crime and optimise resource allocation.
+          <span style={{color:'rgb(13, 0, 107)'}} className='font-bold text-lg'>Predct<span className='text-red-700'>X</span></span> is a revolutionary police performance management and predictive crime analysis tool. We leverage the power of data science and artificial intelligence to equip law enforcement agencies with the insights they need to proactively prevent crime and optimise resource allocation.
           </p>
           <p className='text-black font-medium text-justify'>
           We empower law enforcement with the tools they need to not just analyse past crime, but predict future criminal activity and optimise police resources.
           </p>
-          <p className='text-black font-medium text-justify'>
+          <p className='text-black font-bold text-lg text-justify'>
           Our Mission:
           </p>
           <p className='text-black font-medium text-justify'>
