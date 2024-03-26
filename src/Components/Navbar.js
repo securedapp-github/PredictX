9faa7f12775eb0c825b8c71a48c95c8bc934f70d
Naveen-gn <naveen2003gn@gmail.com>
2024-03-26 23:16:54 +0530

    navbar hover color fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import Predctx from '../Images/Predctx.png';
 
 function NavBar() {
   return (
-      <Navbar >
+      <Navbar>
         <Navbar.Brand className="z-50">
           <img src={Predctx} className="ml-10 h-6 sm:h-9" alt="Flowbite React Logo" />
         </Navbar.Brand>
@@ -12,11 +12,11 @@ function NavBar() {
           <Navbar.Toggle  className="z-50 text-white bg-blue-600  focus:outline-none active:outline-none focus:bg-blue-600"/>
         </div>
         <Navbar.Collapse className="z-50 border-none">
-          <Navbar.Link href="#" className="z-50 text-black text-md border-none font-semibold">Home</Navbar.Link>
-          <Navbar.Link href="#" className="z-50 text-black text-md border-none font-semibold">Map</Navbar.Link>
-          <Navbar.Link href="#" className="z-50 text-black text-md border-none font-semibold">Profile</Navbar.Link>
-          <Navbar.Link href="#" className="z-50 text-black text-md border-none font-semibold">Crime</Navbar.Link>
-          <Navbar.Link href="#" className="z-50 text-black text-md border-none font-semibold">Contact</Navbar.Link>
+          <Navbar.Link href="#" className="z-50 text-black text-md border-none font-semibold hover:text-blue-800" style={{ transition: 'color 0.2s', color: 'black' }} onMouseOver={(e) => e.target.style.color = 'blue'} onMouseOut={(e) => e.target.style.color = 'black'}>Home</Navbar.Link>
+          <Navbar.Link href="#" className="z-50 text-black text-md border-none font-semibold hover:text-blue-800" style={{ transition: 'color 0.2s', color: 'black' }} onMouseOver={(e) => e.target.style.color = 'blue'} onMouseOut={(e) => e.target.style.color = 'black'}>Map</Navbar.Link>
+          <Navbar.Link href="#" className="z-50 text-black text-md border-none font-semibold hover:text-blue-800" style={{ transition: 'color 0.2s', color: 'black' }} onMouseOver={(e) => e.target.style.color = 'blue'} onMouseOut={(e) => e.target.style.color = 'black'}>Profile</Navbar.Link>
+          <Navbar.Link href="#" className="z-50 text-black text-md border-none font-semibold hover:text-blue-800" style={{ transition: 'color 0.2s', color: 'black' }} onMouseOver={(e) => e.target.style.color = 'blue'} onMouseOut={(e) => e.target.style.color = 'black'}>Crime</Navbar.Link>
+          <Navbar.Link href="#" className="z-50 text-black text-md border-none font-semibold hover:text-blue-800" style={{ transition: 'color 0.2s', color: 'black' }} onMouseOver={(e) => e.target.style.color = 'blue'} onMouseOut={(e) => e.target.style.color = 'black'}>Contact</Navbar.Link>
         </Navbar.Collapse>
       </Navbar>
 
