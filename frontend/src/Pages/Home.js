50e9bb62b376c30587b413564234caf5891f428d
Naveen-gn <naveen2003gn@gmail.com>
2024-03-29 09:23:36 +0530

    Custom color added and changed all color

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -23,19 +23,17 @@ export default function Home() {
       <div className='flex '>
       <div className='sm:h-screen h-10/12 '><img src={Homeicon} alt="Homeicon" className='absolute hidden sm:block top-0 right-0 h-2/4 w-56 sm:h-4/5  sm:min-w-80 md:h-full md:w-2/4 lg:w-2/5 xl:w-2/5 2xl:w-1/3'  /></div>
       <div  className='  xl:w-3/4 sm:w-3/5 lg:w-3/5 z-50 sm:ml-3 md:ml-2  '>
-      <h1 className=' font-bold text-4xl sm:text-2xl sm:ml-4 lg:text-3xl lg:ml-36 xl:text-4xl xl:ml-36 2xl:text-4xl flex flex-col justify-center text-center mt-20 ' style={{color:'rgb(13, 0, 107)'}}> <span>Welcome to the world of <span className='text-red-700'>Predictive</span></span> <span className='text-red-700'>crime analysis</span></h1>
+      <h1 className=' font-bold text-4xl sm:text-2xl sm:ml-4 lg:text-3xl lg:ml-36 xl:text-4xl xl:ml-36 2xl:text-4xl flex flex-col justify-center text-center mt-20 text-mybg'> <span>Welcome to the world of <span className='text-red-700'>Predictive</span></span> <span className='text-red-700'>crime analysis</span></h1>
 <div className='w-11/12 ml-5 flex flex-col mt-12 gap-12'>
 <div className='mx-auto flex gap-2 justify-center flex-wrap border border-slate-400 shadow-md p-4 rounded-2xl items-center'>
     <button  className='bg-white text-black border border-slate-400 shadow-md flex justify-center items-center p-3 rounded-xl'
      ><img src={Location} alt="" className='mr-2' />Select Location<img src={Polygon} alt="" className='ml-3' /></button>
 <input type="date" name="date" id="date" className='bg-white text-black border border-slate-400 shadow-md rounded-xl cursor-pointer' placeholder='Select Date' />
-<button  className=' text-white border border-slate-400 shadow-md flex justify-center items-center p-3 px-8 rounded-xl'
-    style={{backgroundColor:'rgb(13, 0, 107)'}}
+<button  className=' text-white border border-slate-400 shadow-md flex justify-center items-center p-3 px-8 rounded-xl bg-mybg'
     >Search</button>
     </div>
     <div className='mx-auto flex gap-4'>
-    <button  className=' text-white border border-slate-400 shadow-md flex justify-center items-center p-3 px-8 rounded-xl'
-    style={{backgroundColor:'rgb(13, 0, 107)'}}
+    <button  className=' text-white border border-slate-400 shadow-md flex justify-center items-center p-3 px-8 rounded-xl bg-mybg'
     >View</button>
     <button  className=' text-black font-semibold border bg-slate-400 border-slate-400 shadow-md flex justify-center items-center p-3 px-8 rounded-xl'
     >Learn More</button>
@@ -86,7 +84,7 @@ export default function Home() {
         <div id='left' className='w-full  sm:w-10/12 lg:w-9/12 2xl:w-1/2  flex justify-center '>
 
           <div className='w-full lg:w-10/12 bg-slate-200 xl:w-9/12 '>
-            <div style={{backgroundColor:'rgb(13, 0, 107)'}} className='p-4 flex  justify-center items-center gap-3'>
+            <div  className='p-4 flex  justify-center items-center gap-3 bg-mybg'>
             <img src={Codicon} alt="" />
             <h1 className='font-semibold  text-white text-xl text-center ' >Latest Notifications</h1>
           </div>
