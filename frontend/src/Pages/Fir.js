936677631ae446fd9ff4fbf87fdec7496561353b
Naveen-gn <naveen2003gn@gmail.com>
2024-04-08 14:57:47 +0530

    Fir page completed

--- frontend/src/Pages/Fir.js
+++ frontend/src/Pages/Fir.js
@@ -1,9 +1,17 @@
-import React from 'react'
-import { Button } from "flowbite-react";
+import React, { useState } from 'react'
+import { Button,Table,Pagination } from "flowbite-react";
 import { Link } from "react-router-dom";
 import Submit from '../Images/Icons/Submit.png';
+import Audio from '../Images/Icons/Audio.png';
+import Video from '../Images/Icons/Video.png';
+import Pdf from '../Images/Icons/Pdf.png';
+import Movie from '../Images/Icons/Movie.png';
+import { FaDownload } from 'react-icons/fa';
 
 export default function Fir() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const onPageChange = (page: number) => setCurrentPage(page);
   return (
     <div className='w-full'>
         
@@ -13,6 +21,103 @@ export default function Fir() {
             <p className='text-2xl font-medium text-black md:ml-6'>Do you want to Submit FIR? <br /> <span className='  text-sm font-normal'>Your file will be provided as safe and secure</span></p>
             <Button color='blue' className="mr-10 rounded-lg bg-mybg z-50 h-12"><Link to='/signin' className='flex justify-center items-center gap-2'><img src={Submit} alt='' /> Submit FIR</Link></Button>
         </div>
+        <h1 className='font-medium text-2xl ml-4 md:ml-10  mt-10'>Explore by format</h1>
+
+      <div className='w-full mt-7 flex flex-col lg:flex-row lg:justify-between lg:gap-0  items-center gap-4 '>
+      <Button color='gray' className=" rounded-lg bg-slate-200 border-2 border-slate-300 z-50 h-24 w-72 flex justify-start"><Link to='/signin' className='flex items-center gap-4 text-mybg font-medium text-lg'><img src={Audio} className='h-10 w-8' alt='' /><p className='text-start'>Audio files <br /><span className='text-black text-sm'>size:15 GB</span></p></Link></Button>
+
+      <Button color='gray' className=" rounded-lg bg-slate-200 border-2 border-slate-300 z-50 h-24 w-72 flex justify-start"><Link to='/signin' className='flex items-center gap-4 text-mybg font-medium text-lg'><img src={Video} className='h-10 w-8' alt='' /><p className='text-start'>Video files <br /><span className='text-black text-sm'>size:48 GB</span></p></Link></Button>
+
+      <Button color='gray' className=" rounded-lg bg-slate-200 border-2 border-slate-300 z-50 h-24 w-72 flex justify-start"><Link to='/signin' className='flex items-center gap-4 text-mybg font-medium text-lg'><img src={Pdf} className='h-10 w-8' alt='' /><p className='text-start'>Pdf files <br /><span className='text-black text-sm'>size:10 GB</span></p></Link></Button>
+      </div>
+      <h1 className='font-medium text-2xl ml-4 md:ml-10  mt-10'>All Files</h1>
+
+
+      <div className="overflow-x-auto mt-10">
+      <Table hoverable>
+        <Table.Head className='border-b-2 border-b-slate-400'>
+          <Table.HeadCell>Filename</Table.HeadCell>
+          <Table.HeadCell>Date</Table.HeadCell>
+          <Table.HeadCell>State</Table.HeadCell>
+          <Table.HeadCell>Location</Table.HeadCell>
+          <Table.HeadCell>Type</Table.HeadCell>
+        </Table.Head>
+        <Table.Body className="divide-y">
+
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white flex gap-4">
+             <img src={Movie} alt="" className='h-4 w-6' /> {'Evidence of kalar theft case'}
+            </Table.Cell>
+            <Table.Cell className='text-nowrap ' >02-04-2024</Table.Cell >
+            <Table.Cell className='text-nowrap ' >Kalar</Table.Cell >
+            <Table.Cell className='text-nowrap ' >Bangalore-kmk</Table.Cell>
+            <Table.Cell  className='flex items-center gap-6'>
+            <Button color="failure" outline>View</Button>
+            <button><FaDownload color="#0d006b" /></button>
+            </Table.Cell>
+          </Table.Row>
+          
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white flex gap-4">
+             <img src={Movie} alt="" className='h-4 w-6' /> {'Evidence of kalar theft case'}
+            </Table.Cell>
+            <Table.Cell className='text-nowrap ' >02-04-2024</Table.Cell >
+            <Table.Cell className='text-nowrap ' >Kalar</Table.Cell >
+            <Table.Cell className='text-nowrap ' >Bangalore-kmk</Table.Cell>
+            <Table.Cell  className='flex items-center gap-6'>
+            <Button color="failure" outline>View</Button>
+            <button><FaDownload color="#0d006b" /></button>
+            </Table.Cell>
+          </Table.Row>
+          
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white flex gap-4">
+             <img src={Movie} alt="" className='h-4 w-6' /> {'Evidence of kalar theft case'}
+            </Table.Cell>
+            <Table.Cell className='text-nowrap ' >02-04-2024</Table.Cell >
+            <Table.Cell className='text-nowrap ' >Kalar</Table.Cell >
+            <Table.Cell className='text-nowrap ' >Bangalore-kmk</Table.Cell>
+            <Table.Cell  className='flex items-center gap-6'>
+            <Button color="failure" outline>View</Button>
+            <button><FaDownload color="#0d006b" /></button>
+            </Table.Cell>
+          </Table.Row>
+          
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white flex gap-4">
+             <img src={Movie} alt="" className='h-4 w-6' /> {'Evidence of kalar theft case'}
+            </Table.Cell>
+            <Table.Cell className='text-nowrap ' >02-04-2024</Table.Cell >
+            <Table.Cell className='text-nowrap ' >Kalar</Table.Cell >
+            <Table.Cell className='text-nowrap ' >Bangalore-kmk</Table.Cell>
+            <Table.Cell  className='flex items-center gap-6'>
+            <Button color="failure" outline>View</Button>
+            <button><FaDownload color="#0d006b" /></button>
+            </Table.Cell>
+          </Table.Row>
+          
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white flex gap-4">
+             <img src={Movie} alt="" className='h-4 w-6' /> {'Evidence of kalar theft case'}
+            </Table.Cell>
+            <Table.Cell className='text-nowrap ' >02-04-2024</Table.Cell >
+            <Table.Cell className='text-nowrap ' >Kalar</Table.Cell >
+            <Table.Cell className='text-nowrap ' >Bangalore-kmk</Table.Cell>
+            <Table.Cell  className='flex items-center gap-6'>
+            <Button color="failure" outline>View</Button>
+            <button><FaDownload color="#0d006b" /></button>
+            </Table.Cell>
+          </Table.Row>
+          
+        </Table.Body>
+      </Table>
+    </div>
+
+
+    <div className="flex overflow-x-auto sm:justify-center mt-10 ">
+      <Pagination currentPage={currentPage} totalPages={100} onPageChange={onPageChange} showIcons color='blue' />
+    </div>
+
 
       </div>
      
